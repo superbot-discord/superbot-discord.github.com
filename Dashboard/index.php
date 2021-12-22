49644d5d80eaf9c5a49d75bce3d31cd3577f43a4
johann-lau <johannlau8888@gmail.com>
2021-12-22 12:37:18 +0800

    Fixed PHP

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-  <title>Why SuperBot</title>
+  <title>SuperBot Dashboard</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" id="mystyle" href="/style.css">
   <base target="_blank">
